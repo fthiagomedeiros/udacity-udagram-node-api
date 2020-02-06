9153ee7e48ce1a05fb83582ee5cfcd5a55a64d6a
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-02-05 18:39:23 -0800

    Udacity Nanodegree project 2

---
+++ src/controllers/v0/feed/routes/feed.router.ts
@@ -0,0 +1,93 @@
+import { Router, Request, Response } from 'express';
+import { FeedItem } from '../models/FeedItem';
+import { requireAuth } from '../../users/routes/auth.router';
+import * as AWS from '../../../../aws';
+
+const router: Router = Router();
+
+// Get all feed items
+router.get('/', async (req: Request, res: Response) => {
+    const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
+    items.rows.map((item) => {
+            if(item.url) {
+                item.url = AWS.getGetSignedUrl(item.url);
+            }
+    });
+    res.send(items);
+});
+
+//Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+
+    // Retrieve the tag from our URL path
+    let { id } = req.params;
+    console.log(" from get " + id);
+
+    const items = await FeedItem.findByPk(id);
+    res.send(items);
+});
+
+// update a specific resource
+router.patch('/:id', 
+    requireAuth, 
+    async (req: Request, res: Response) => {
+
+        // Retrieve the tag from our URL path
+        let { id } = req.params;
+        console.log(" from patch " + id);
+
+        const feed_update = req.body;
+        console.log(" from feed update " + feed_update);
+
+        const items = await FeedItem.findByPk(id);
+        console.log(" from feed update " + items);
+
+        items.caption = feed_update.caption;
+        await items.save();
+
+        return res
+            .status(200)
+            .json({ message: "Updated Succesfully", data: items });
+});
+
+
+// Get a signed url to put a new item in the bucket
+router.get('/signed-url/:fileName', 
+    requireAuth, 
+    async (req: Request, res: Response) => {
+    let { fileName } = req.params;
+    const url = AWS.getPutSignedUrl(fileName);
+    res.status(201).send({url: url});
+});
+
+// Post meta data and the filename after a file is uploaded 
+// NOTE the file name is they key name in the s3 bucket.
+// body : {caption: string, fileName: string};
+router.post('/', 
+    requireAuth, 
+    async (req: Request, res: Response) => {
+    const caption = req.body.caption;
+    const fileName = req.body.url;
+
+    // check Caption is valid
+    if (!caption) {
+        return res.status(400).send({ message: 'Caption is required or malformed' });
+    }
+
+    // check Filename is valid
+    if (!fileName) {
+        return res.status(400).send({ message: 'File url is required' });
+    }
+
+    const item = await new FeedItem({
+            caption: caption,
+            url: fileName
+    });
+
+    const saved_item = await item.save();
+
+    saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+    res.status(201).send(saved_item);
+});
+
+export const FeedRouter: Router = router;
